fe2db83b143867be92989f6bf42b1be7734ca641
twirtgen <thomas.wirtgen@uclouvain.be>
2020-11-02 14:53:04 +0100

    Merging Base repo to this new one

--- xbgp_compliant_api/xbgp_plugin_api.h
+++ xbgp_compliant_api/xbgp_plugin_api.h
@@ -8,6 +8,71 @@
 #include "ubpf_api_common.h"
 #include "ubpf_prefix.h"
 
+
+/**
+ * Insertion point that must be
+ * implemented on each BGP implementation
+ */
+enum ubpf_plugins {
+    BGP_UNUSED = 0 ,
+    BGP_PRE_DECISION,
+    BGP_NEXTHOP_RESOLVABLE_DECISION,
+    BGP_LOCAL_PREF_DECISION,
+    BGP_AS_PATH_LENGTH_DECISION,
+    BGP_MED_DECISION, // decision process MED insertion point
+    BGP_USE_ORIGIN_DECISION,
+    BGP_PREFER_EXTERNAL_PEER_DECISION,
+    BGP_IGP_COST_DECISION,
+    BGP_ROUTER_ID_DECISION,
+    BGP_IPADDR_DECISION,
+    BGP_POST_DECISION,
+    BGP_DECODE_ATTR,
+    BGP_ENCODE_ATTR,
+    BGP_PRE_INBOUND_FILTER,
+    BGP_PRE_OUTBOUND_FILTER,
+};
+
+
+enum BGP_ROUTE_TYPE {
+    BGP_ROUTE_TYPE_UNDEF = 0,
+    BGP_ROUTE_TYPE_NEW,
+    BGP_ROUTE_TYPE_OLD,
+};
+
+/* REGISTERED ATTRIBUTE ID */
+
+#define RESERVED_ATTR_ID 0
+#define ORIGIN_ATTR_ID 1
+#define AS_PATH_ATTR_ID 2
+#define NEXT_HOP_ATTR_ID 3
+#define MULTI_EXIT_DISC_ATTR_ID 4
+#define LOCAL_PREF_ATTR_ID 5
+#define ATOMIC_AGGREGATE_ATTR_ID 6
+#define AGGREGATOR_ATTR_ID 7
+#define COMMUNITY_ATTR_ID 8
+#define ORIGINATOR_ID_ATTR_ID 9
+#define CLUSTER_LIST_ATTR_ID 10
+#define MP_REACH_NLRI_ATTR_ID 14
+#define MP_UNREACH_NLRI_ATTR_ID 15
+#define EXTENDED_COMMUNITIES_ATTR_ID 16
+#define AS4_PATH_ATTR_ID 17
+#define AS4_AGGREGATOR_ATTR_ID 18
+#define PMSI_TUNNEL_ATTR_ID 22
+#define TUNNEL_ENCAPSULATION_ATTRIBUTE_ATTR_ID 23
+#define TRAFFIC_ENGINEERING_ATTR_ID 24
+#define IPV6_ADDRESS_SPECIFIC_EXTENDED_COMMUNITY_ATTR_ID 25
+#define AIGP_ATTR_ID 26
+#define PE_DISTINGUISHER_LABELS_ATTR_ID 27
+#define BGPLS_ATTRIBUTE_ATTR_ID 29
+#define LARGE_COMMUNITY_ATTR_ID 32
+#define BGPSEC_PATH_ATTR_ID 33
+#define BGP_COMMUNITY_CONTAINER_ATTRIBUTE_TEMPORARY_REGISTERED_20170728_ATTR_ID 34
+#define ONLY_TO_CUSTOMER_OTC_TEMPORARY_REGISTERED_20180329_ATTR_ID 35
+#define BGP_DOMAIN_PATH_DPATH_TEMPORARY_REGISTERED_20190708_ATTR_ID 36
+#define SFP_ATTRIBUTE_ATTR_ID 37
+#define BGP_PREFIXSID_ATTR_ID 40
+#define ATTR_SET_ATTR_ID 128
+
 /**
  * Adds a new attribute to the route processed by the current plugin
  * @param code The number that identify the attribute type
@@ -159,6 +224,15 @@ extern struct ubpf_rib_entry *get_rib_in_entry(uint8_t af_family, union ubpf_pre
 extern struct ubpf_rib_entry *get_rib_out_entry(uint8_t af_family, union ubpf_prefix *pfx);
 
 extern struct ubpf_rib_entry *get_loc_rib_entry(uint8_t af_family, union ubpf_prefix *pfx);
+
+/**
+ * Retrieve the current BGP route being processed in the insertion point
+ *
+ * @param which_route Given the Insertion point (and if multiple route are involved), select the route to be
+ *                    loaded in the VM.
+ * @return The BGP route
+ */
+extern struct bgp_route *get_bgp_route(enum BGP_ROUTE_TYPE type);
 
 #endif //PLUGINIZED_BIRD_XBGP_API_H
 
