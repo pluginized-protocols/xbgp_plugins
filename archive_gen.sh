40b7d756acfea43cc9ff6b0a32147dadf7b06b79
twirtgen <thomas.wirtgen@uclouvain.be>
2022-01-10 13:38:10 +0100

    Reformat script

--- archive_gen.sh
+++ archive_gen.sh
@@ -1,18 +1,16 @@
 #! /bin/bash -e
 
-info () {
-    echo -e "[INFO] ${*}"
+info() {
+  echo -e "[INFO] ${*}"
 }
 
-list_dir () {
+list_dir() {
   find "$1" -type d ! -path '*docs*' ! -path '*git*' ! -path '*venv*' ! -path '*cmake*' ! -path '*idea*' ! -path '*xbgp_compliant_api*' ! -path '*prove_stuffs*'
 }
 
 mapfile -t < <(list_dir . | sed -E "s/^\.$//g")
-for PLUGIN in "${MAPFILE[@]}"
-do
-  if [ -n "$PLUGIN" ]
-  then
+for PLUGIN in "${MAPFILE[@]}"; do
+  if [ -n "$PLUGIN" ]; then
     tar cjf "${PLUGIN}.tar.bz2" -h "${PLUGIN}" xbgp_compliant_api/*.h prove_stuffs/{*.h,*.c} ./*.h
     info "${PLUGIN} archived."
   fi
